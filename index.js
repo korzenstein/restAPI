1a8720564eab73ff417c8b44e6dc240494716691
Stephen Korzenstein <skorzenstein@gmail.com>
2022-05-31 20:10:34 -0400

    initial commit with base code for express

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.listen(PORT, () => console.log(`Listening to Stephen's server on port: http://localhost:${PORT}`));
